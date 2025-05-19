a5e2f74997a65e347bfd73c421a0fdc413c4d110
HakkiMcSnakki213 <harkirat.grewalhs.hg@gmail.com>
2025-05-19 15:27:13 +1000

    just some merging

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,15 +1,20 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static PauseMenu Instance { get; private set; }
+
     public static bool gamePaused = false;
     public static bool gameOver = false;
 
     public GameObject pauseMenuUI;
     public GameObject gameOverUI;
     public GameObject gameWinUI;
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI score2;
     public GameObject player;
 
     public void Start()
@@ -56,13 +61,16 @@ public class PauseMenu : MonoBehaviour
     {
         gameOverUI.SetActive(true);
         Time.timeScale = 0f;
+        score.text = GameManager.Instance.GetTotalScore().ToString();
         gameOver = true;
     }
 
-    public void GameWin()
+    public void GameWin(float scoreNumber)
     {
         gameWinUI.SetActive(true);
         Time.timeScale = 0f;
+        score2.text = scoreNumber.ToString();
+        gameOver = true;
     }
 
     public void RestartLevel()
