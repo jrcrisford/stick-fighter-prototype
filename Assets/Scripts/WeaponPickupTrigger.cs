6702ae9024eecaa7fcc99b54ea5639dd290a0081
jrc <jc101@utas.edu.au>
2025-05-27 09:31:28 +1000

    Add UI to WeaponPickupTrigger

--- Assets/Scripts/WeaponPickupTrigger.cs
+++ Assets/Scripts/WeaponPickupTrigger.cs
@@ -3,16 +3,55 @@ using UnityEngine;
 [RequireComponent(typeof(MeleeWeapon))]
 public class WeaponPickupTrigger : MonoBehaviour
 {
+    private bool playerInRange = false;
+    private WeaponHandler playerHandler;
     private bool isPickedUp = false;
+
+    [SerializeField] private GameObject pickupUIPrompt;
+
+    private void Start()
+    {
+        pickupUIPrompt = GetComponentInChildren<Canvas>(true)?.gameObject;
+
+        if (pickupUIPrompt != null)
+            pickupUIPrompt.SetActive(false);
+        else
+            Debug.LogWarning("WeaponPickupTrigger: Pickup UI prompt not found as child!");
+    }
+
     private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player") || isPickedUp) return;
+
+        playerHandler = other.GetComponent<WeaponHandler>();
+        if (playerHandler != null)
+        {
+            playerInRange = true;
+            if (pickupUIPrompt != null)
+                pickupUIPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
     {
         if (!other.CompareTag("Player")) return;
 
-        var handler = other.GetComponent<WeaponHandler>();
-        if (handler != null && !isPickedUp)
+        if (pickupUIPrompt != null)
+            pickupUIPrompt.SetActive(false);
+
+        playerInRange = false;
+        playerHandler = null;
+    }
+
+    private void Update()
+    {
+        if (playerInRange && !isPickedUp && Input.GetKeyDown(KeyCode.E))
         {
+            playerHandler.PickupWeapon(GetComponent<MeleeWeapon>());
             isPickedUp = true;
-            handler.PickupWeapon(GetComponent<MeleeWeapon>());
+
+            if (pickupUIPrompt != null)
+                pickupUIPrompt.SetActive(false);
         }
     }
 }
